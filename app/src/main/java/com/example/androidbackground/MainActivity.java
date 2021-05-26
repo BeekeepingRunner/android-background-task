06a7803555c65920c1eace4ce551818ae1ff378b
Bartosz Biegajło <biegbart.z@gmail.com>
2021-05-26 10:01:34 +0200

    add async task stub

--- app/src/main/java/com/example/androidbackground/MainActivity.java
+++ app/src/main/java/com/example/androidbackground/MainActivity.java
@@ -2,9 +2,25 @@ package com.example.androidbackground;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static class DownloadInfoTask extends AsyncTask {
+
+        // Connect with server, get file info (name, type), return FileInfo
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            return null;
+        }
+
+        // display fetched info in GUI
+        @Override
+        protected void onPostExecute(Object o) {
+            super.onPostExecute(o);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
